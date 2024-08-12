13d16259076daf5b90806feb04ce1dd73c20c0cd
quangnhat23 <nguyennhatq22@gmail.com>
2024-08-12 13:48:56 -0700

    finish task_5

--- tasks/task_5/task_5.py
+++ tasks/task_5/task_5.py
@@ -57,7 +57,15 @@ class ChromaCollectionCreator:
         # Use a TextSplitter from Langchain to split the documents into smaller text chunks
         # https://python.langchain.com/docs/modules/data_connection/document_transformers/character_text_splitter
         # [Your code here for splitting documents]
+        text_splitter= CharacterTextSplitter(
+            separator="\n\n",
+            chunk_size= 1000,
+            chunk_overlap= 200,
+            length_function=len,
+            is_separator_regex=False,)
         
+        aux_array=  list(map(lambda page: page.page_content, self.processor.pages))
+        texts=text_splitter.create_documents(aux_array)
         if texts is not None:
             st.success(f"Successfully split pages to {len(texts)} documents!", icon="✅")
 
@@ -66,6 +74,7 @@ class ChromaCollectionCreator:
         # Create a Chroma in-memory client using the text chunks and the embeddings model
         # [Your code here for creating Chroma collection]
         
+        self.db= Chroma.from_documents(texts ,self.embed_model.client,persist_directory="./chroma_db")
         if self.db:
             st.success("Successfully created Chroma Collection!", icon="✅")
         else:
